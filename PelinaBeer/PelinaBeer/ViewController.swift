ca03006cdb28d9412c081b81d7c64968500711d6
Luis Tejada <ltejada@intellisys.com.do>
2020-08-13 19:40:28 -0400

    Creación de proyecto.

---
+++ PelinaBeer/PelinaBeer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PelinaBeer
+//
+//  Created by Intellisys on 8/13/20.
+//  Copyright © 2020 Luis Tejada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
